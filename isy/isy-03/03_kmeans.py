8b1b09f54b2afff07900938cffc76da49e33f433
Robin Mehlitz <robin.mehlitz@beuth-hochschule.de>
2017-12-06 18:29:02 +0100

    Homework 3.2: impl. kmeans inspired by toms code

--- isy/isy-03/03_kmeans.py
+++ isy/isy-03/03_kmeans.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import math
 import sys
+from collections import defaultdict
 
 
 ############################################################
@@ -10,10 +11,16 @@ import sys
 #
 ############################################################
 
+
+##################### Code inspired by Tom Oberhauser ############################################
+
+
 # implement distance metric - e.g. squared distances between pixels
 def distance(a, b):
-    pass
-    # YOUR CODE HERE
+    dis = 0.0
+    for aX, pY in zip(a, b):
+        dis += (aX - pY) ** 2
+    return np.sqrt(dis)
 
 # k-means works in 3 steps
 # 1. initialize
@@ -21,35 +28,52 @@ def distance(a, b):
 # 3. update mean
 # then iterate between 2 and 3 until convergence, i.e. until ~smaller than 5% change rate in the error
 
-def update_mean(img, clustermask):
+def update_mean(img, clustermask, current_cluster_centers):
     """This function should compute the new cluster center, i.e. numcluster mean colors"""
-    # YOUR CODE HERE
-    pass
+    clusterColors = defaultdict(list)
+    img_h, img_w = img.shape[:2]
+    for h in range(0, img_h):
+        for w in range(0, img_w):
+            clusterID = int(clustermask[h][w])
+            color_pixel = img[h][w]
+            clusterColors[clusterID].append(color_pixel)
+    for k in clusterColors.keys():
+        current_cluster_centers[k] = np.uint8(np.mean(clusterColors[k], axis=0))
 
-def assign_to_current_mean(img, result, clustermask):
+def assign_to_current_mean(img, result, clustermask, current_cluster_centers):
     """The function expects the img, the resulting image and a clustermask.
     After each call the pixels in result should contain a cluster_color corresponding to the cluster
     it is assigned to. clustermask contains the cluster id (int [0...num_clusters]
     Return: the overall error (distance) for all pixels to there closest cluster center (mindistance px - cluster center).
     """
     overall_dist = 0
-    # YOUR CODE HERE
+    img_h, img_w = img.shape[:2]
+    for h in range(0, img_h):
+        for w in range(0, img_w):
+            color_pixel = img[h][w]
+            color_mean = current_cluster_centers[clustermask[h][w]]
+            color_mean = np.reshape(color_mean, color_pixel.shape)
+            overall_dist += distance(color_pixel, color_mean)
+            result[h][w] = color_mean
     return overall_dist
 
 
 
 def initialize(img):
     """inittialize the current_cluster_centers array for each cluster with a random pixel position"""
-    # YOUR CODE HERE
-    print(current_cluster_centers)
+    h, w = img.shape[:2]
+    rand_h = int(h * np.random.random())
+    rand_w = int(h * np.random.random())
+    return img[rand_h][rand_w] # return color at random pixel position
 
 
-def kmeans(img):
+def kmeans(img, current_cluster_centers):
     """Main k-means function iterating over max_iterations and stopping if
     the error rate of change is less then 2% for consecutive iterations, i.e. the
     algorithm converges. In our case the overall error might go up and down a little
     since there is no guarantee we find a global minimum.
     """
+    h1, w1 = img.shape[:2]
     max_iter = 10
     max_change_rate = 0.02
     dist = sys.float_info.max
@@ -60,7 +84,36 @@ def kmeans(img):
     # initializes each pixel to a cluster
     # iterate for a given number of iterations or if rate of change is
     # very small
-    # YOUR CODE HERE
+
+    i = 0
+    change_rate = 1.0
+    while i < max_iter and change_rate > max_change_rate:
+        changes = 0
+        for h in range(0,h1):
+            for w in range(0, w1):
+                # get pixel and calculate distances to all current cluster centers
+                pixel = img[h][w]
+                currentClusterID = clustermask[h][w]
+                distances = []
+                # I don´t use PriorityQueue here since it crashes when to values have the same priority
+                # (see last assignment, gave an additional index to the queue to have a second index
+                # for it to order by).
+                for id, cc in enumerate(current_cluster_centers):
+                    cc = np.reshape(cc, pixel.shape)
+                    distances.append((distance(cc, pixel), id))
+
+                closestClusterID = sorted(distances, key=lambda x: x[0])[0][1] # get closest clusterID
+                if closestClusterID != currentClusterID:
+                    # Set id of closest cluster center to clustermask at this position
+                    clustermask[h][w] = closestClusterID
+                    changes+=1
+
+        change_rate = changes / (w1*h1)
+        print('Change rate: ', change_rate)
+        overallError = assign_to_current_mean(img, result, clustermask, current_cluster_centers)
+        #update_mean(img, clustermask, current_cluster_centers)
+        print('Error: ', overallError)
+        i+=1
 
     return result
 
@@ -79,12 +132,20 @@ imgraw = cv2.resize(imgraw, None, fx=scaling_factor, fy=scaling_factor, interpol
 # compare different color spaces and their result for clustering
 # YOUR CODE HERE or keep going with loaded RGB colorspace img = imgraw
 image = imgraw
+# image = cv2.cvtColor(imgraw, cv2.COLOR_BGR2YUV)
+# image = cv2.cvtColor(imgraw, cv2.COLOR_BGR2LAB)
+image = cv2.cvtColor(imgraw, cv2.COLOR_BGR2HSV)
+
+for i in range(0, numclusters):
+    current_cluster_centers[i] = initialize(image)
+
 h1, w1 = image.shape[:2]
 
 # execute k-means over the image
 # it returns a result image where each pixel is color with one of the cluster_colors
 # depending on its cluster assignment
-res = kmeans(image)
+# res = kmeans(image)
+res = kmeans(image, current_cluster_centers)
 
 h1, w1 = res.shape[:2]
 h2, w2 = image.shape[:2]
@@ -95,3 +156,6 @@ vis[:h2, w1:w1 + w2] = image
 cv2.imshow("Color-based Segmentation Kmeans-Clustering", vis)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+#TODO check https://mubaris.com/2017-10-01/kmeans-clustering-in-python
