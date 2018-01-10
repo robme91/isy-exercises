239a399ec20f526c5b2cab2602633c7322184126
Robin Mehlitz <robin.mehlitz@beuth-hochschule.de>
2018-01-10 21:08:22 +0100

    Exercise 4.2: added more result Exercise 4.1: finished the svm, done

--- isy/isy-04/04_svm.py
+++ isy/isy-04/04_svm.py
@@ -14,8 +14,7 @@ from sklearn import svm
 def create_keypoints(w, h):
     keypoints = []
     keypointSize = 15
-    # Erhöhung der KPs bringt nicht wirklich mehr erfolg...
-    kpsPerDirection = 25
+    kpsPerDirection = 196   # just working because all images got the same size. if they are not, dimension error occurs at filling x_train
     stepDistW = w / kpsPerDirection
     stepDistH = h / kpsPerDirection
     y = 0
@@ -50,25 +49,40 @@ sift = cv2.xfeatures2d.SIFT_create()
 # You also need a y_train vector containing the labels encoded as integers
 
 labels = {1: 'car', 2: 'face', 3: 'flower'}
-x_train = np.array(1)
-y_train = np.array(1)
-# TODO eigene liste nur mit bild und passender nummer, dann durch liste gehen und label und descriptors übernehmen. so kann das erste element für y_train und x_train einfach aus der train liste herausgenommen werden
+trainImages = list()
+print("Read images and assign them to their labels")
 for labelNum, labelCaption in labels.items():
-    trainImages = glob.glob('./images/db/train/{}/*.jpg'.format(labelCaption + 's'))
-    for imgPath in trainImages:
+    imgPaths = glob.glob('./images/db/train/{}/*.jpg'.format(labelCaption + 's'))
+    for imgPath in imgPaths:
         img = cv2.imread(imgPath, 1)
-        x_train = np.vstack((x_train, create_descriptor(img).ravel()))
-        y_train = np.vstack((y_train, labelNum))
+        trainImages.append((img, labelNum))
+print("Done with image gold classification")
 
-print("try")
-# TODO mcreate x_train and y_train with the right shape so vstack make sense
+print("Start computing all descriptors for every image and build the training vectors")
+# to get the x_train shape get descriptor of the first image
+X_train = np.asmatrix(create_descriptor(trainImages[0][0]).ravel())
+y_train = trainImages[0][1]
+for img, imgClass in trainImages[1:]:
+    X_train = np.vstack((X_train, create_descriptor(img).ravel()))
+    y_train = np.vstack((y_train, imgClass))
+print("Done, building training vectors")
 
 # 3. We use scikit-learn to train a SVM classifier. Specifically we use a LinearSVC in our case. Have a look at the documentation.
 # You will need .fit(X_train, y_train)
-
-
+print("Now train that shizzle")
+classifier = svm.LinearSVC()
+classifier.fit(X_train, y_train.ravel())    # because sklearn asks for, y_train is raveld again
+print("Ahhh that was a good workout. Finished the training!")
 # 4. We test on a variety of test images ./images/db/test/ by extracting an image descriptor
 # the same way we did for the training (except for a single image now) and use .predict()
 # to classify the image
-
 # 5. output the class + corresponding name
+print("Now, let's verify! Predict the test images")
+testImages = glob.glob('./images/db/test/*.jpg')
+for testImgPath in testImages:
+    testImg = cv2.imread(testImgPath, 1)
+    testDesc = create_descriptor(testImg).ravel()
+    pred = classifier.predict([testDesc])
+    print("The image {} is predicted as {}, which means {}.".format(testImgPath, pred[0], labels.get(pred[0])))
+
+
